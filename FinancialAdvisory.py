af0b2b815cbad248edaaf76700a685f3196a741d
ShanksDLAw <aabduljalaal@yahoo.com>
2023-05-17 21:53:38 +0100

    Update FinancialAdvisory.py

--- FinancialAdvisory.py
+++ FinancialAdvisory.py
@@ -1,45 +1,34 @@
-#The essence of this python script is to build a V2 FInancial Mentory Service
-#This script takes a user input in a prompt 
-#From this script it takes the user input and queries the internet via Google Serper API
-#After the search it displays the result
-
-#Importing the Packages
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.llms import OpenAI
 import os
 import streamlit as st
 import openai
 from dotenv import load_dotenv, find_dotenv
-from langchain.agents import initialize_agent, AgentType
 from langchain.llms import OpenAI as LangchainOpenAI
 from langchain.schema import SystemMessage, HumanMessage
-from langchain.utilities import GoogleSerperAPIWrapper
 from langchain.agents import Tool
 from langchain.chat_models import ChatOpenAI
-
-
+from langchain.utilities import GoogleSerperAPIWrapper
 # Load environment variables
-#Input your OpenAI API key in an .env
 _ = load_dotenv(find_dotenv())
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-
-#Displaying the intial UI/UX
+# Displaying the initial UI/UX
 st.title("Symply Finance")
 st.write("We are your AI-powered Financial Wizard")
 st.subheader("Here's some additional information:")
-# Add some more content
 st.write("This app provides general information about investing in financial markets.")
 st.write("It's important to do your own research and consider professional advice before making any investment decisions.")
 
-
-#Collecting prompt from our users
+# Collecting prompt from our users
 prompt = st.text_input('What would you like to know or explained')
 
 # Initialize the Langchain LLM and the Temperature (How creative the AI will be)
 llm = LangchainOpenAI(temperature=0.1)
 
-
 # Initializing the Google Serper API Wrapper tool
-# You can get your serper api by opening an account at serper.dev
 search = GoogleSerperAPIWrapper(api_key=os.getenv('SERPER_API_KEY'))
 
 # Defining use of the Google SerperAPI Wrapper tool
@@ -51,7 +40,9 @@ tools = [
     )
 ]
 
-#Setting up the chat model using ChatOpenAI mode
+# Initialize the tools and agent
+tools = load_tools(["google-serper"], llm=llm)
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
 # Set up ChatOpenAI model
 chat = ChatOpenAI(temperature=0)
@@ -62,26 +53,20 @@ investment_system_messages = [
     SystemMessage(content="Investing involves risks. It's important to do thorough research and consider professional advice."),
     SystemMessage(content="I can provide general and detailed information about investment strategies and concepts."),
     SystemMessage(content="You Provide highly profitable Financial/Investment Strategies")
-
 ]
 
 # Processing user input and display results
 if st.button("Submit"):
     if prompt:
-        user_input = prompt  # Directly use the user's input
+        user_input = prompt
         conversation = [
             *investment_system_messages,
             HumanMessage(content=user_input)
         ]
-        result = chat(conversation)
-        response_text = result.content
+        result = agent(conversation)
+        response_text = result['output']
         st.write(response_text)
     else:
         st.write("Please enter a question.")
-
-
-
-
-
 
 
